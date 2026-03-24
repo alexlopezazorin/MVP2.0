3c836f8c1003b658e02d02e294fc90345afcf24d
alexlopezazorin <alexlopezazorin@gmail.com>
2026-03-24 20:46:18 +0100

    Update logout.ts

--- features/auth/actions/logout.ts
+++ features/auth/actions/logout.ts
@@ -1,1 +1,12 @@
+"user server"
 
+import {createSupabaseServer} from "@/lib/supabaseServer"
+import {redirect} from "next/navigation"
+
+export async function logout() {
+  const supabase = createSupabaseServer()
+
+  await supabase.auth.signOut()
+
+  redirect("/")
+}
