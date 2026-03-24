8368806007ac0e2947fdfd63268a3880b99a57e1
alexlopezazorin <alexlopezazorin@gmail.com>
2026-03-24 20:33:52 +0100

    Update login.ts

--- features/auth/actions/login.ts
+++ features/auth/actions/login.ts
@@ -1,1 +1,22 @@
+"use server"
 
+import { createSupabaseServer } from "@/lib/supabaseServer"
+import { redirect } from "next/navigation"
+
+export async function login(formData: FormData) {
+  const supabase = createSupabaseServer()
+
+  const email = formData.get("email") as string
+  const password = formData.get("password") as string
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+
+  if (error) {
+    return { error: error.message }
+  }
+
+  redirect("/dashboard")
+}
